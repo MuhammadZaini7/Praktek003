e0b40328c194f3f2fa26d4b9baf4d3fd2ea72150
MuhammadZaini7 <32381402+MuhammadZaini7@users.noreply.github.com>
2018-04-10 13:51:51 -0700

    membuat method cetakInfo()

--- src/praktek003/Handphone.java
+++ src/praktek003/Handphone.java
@@ -4,4 +4,9 @@ public class Handphone {
     String merk;
     String warna;
     Double harga;
+    
+    void cetakInfo(){
+       System.out.println("Merk \t :" +merk+"\n"+"Warna \t"
+               + " :" +warna+"\n"+"harga \t :" +harga);
+    }
 }
