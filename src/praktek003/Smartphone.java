fa824806f9b7c1e20a550d1fead5425bf802b3bf
MuhammadZaini7 <32381402+MuhammadZaini7@users.noreply.github.com>
2018-04-10 14:17:20 -0700

    Getter and Setter

--- src/praktek003/Smartphone.java
+++ src/praktek003/Smartphone.java
@@ -7,4 +7,14 @@ public class Smartphone extends Handphone{
     void throttle (){
         System.out.println("Juaranya Handphon...");
     }
+
+    public String getJenisSmartphone() {
+        return jenisSmartphone;
+    }
+
+    public void setJenisSmartphone(String jenisSmartphone) {
+        this.jenisSmartphone = jenisSmartphone;
+    }
+    
+    
 }
