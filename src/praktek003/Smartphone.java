a12fd80e6b6430094de6f296a114dd3b4522c928
MuhammadZaini7 <32381402+MuhammadZaini7@users.noreply.github.com>
2018-04-10 14:16:23 -0700

    attribute dan method

--- src/praktek003/Smartphone.java
+++ src/praktek003/Smartphone.java
@@ -2,5 +2,9 @@
 package praktek003;
 
 public class Smartphone extends Handphone{
-    
+    private String jenisSmartphone;
+     
+    void throttle (){
+        System.out.println("Juaranya Handphon...");
+    }
 }
